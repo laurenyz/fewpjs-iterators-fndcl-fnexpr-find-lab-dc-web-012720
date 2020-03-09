648c9028c5bffefeb68e29c60476906b452fcb31
Lauren Yu <laurenkyu@gmail.com>
2020-03-09 19:37:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
-}
-
-function superbowlWin(array) {
-  array.find(function(s) {
-    s.result === "W"
-    })
-}
-
 superbowlWin = (record) => {
   let result = record.find( record => record.result === "W" )
   return !!result ? result.year : undefined
